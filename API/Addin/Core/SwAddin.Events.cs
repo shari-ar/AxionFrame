d779b002a5a78a3230717937d542fbc9fed662a7
Shahriyar <156581474+shari-ar@users.noreply.github.com>
2026-02-25 15:16:04 +0330

    Refactor SwAddin into organized partial class files

---
+++ API/Addin/Core/SwAddin.Events.cs
@@ -0,0 +1,176 @@
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using System;
+
+namespace AxionFrame
+{
+    public partial class SwAddin
+    {
+        #region Event Methods
+        public bool AttachEventHandlers()
+        {
+            AttachSwEvents();
+            //Listen for events on all currently open docs
+            AttachEventsToAllDocuments();
+            return true;
+        }
+
+        private bool AttachSwEvents()
+        {
+            try
+            {
+                SwEventPtr.ActiveDocChangeNotify += new DSldWorksEvents_ActiveDocChangeNotifyEventHandler(OnDocChange);
+                SwEventPtr.DocumentLoadNotify2 += new DSldWorksEvents_DocumentLoadNotify2EventHandler(OnDocLoad);
+                SwEventPtr.FileNewNotify2 += new DSldWorksEvents_FileNewNotify2EventHandler(OnFileNew);
+                SwEventPtr.ActiveModelDocChangeNotify += new DSldWorksEvents_ActiveModelDocChangeNotifyEventHandler(OnModelChange);
+                SwEventPtr.FileOpenPostNotify += new DSldWorksEvents_FileOpenPostNotifyEventHandler(FileOpenPostNotify);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        private bool DetachSwEvents()
+        {
+            try
+            {
+                SwEventPtr.ActiveDocChangeNotify -= new DSldWorksEvents_ActiveDocChangeNotifyEventHandler(OnDocChange);
+                SwEventPtr.DocumentLoadNotify2 -= new DSldWorksEvents_DocumentLoadNotify2EventHandler(OnDocLoad);
+                SwEventPtr.FileNewNotify2 -= new DSldWorksEvents_FileNewNotify2EventHandler(OnFileNew);
+                SwEventPtr.ActiveModelDocChangeNotify -= new DSldWorksEvents_ActiveModelDocChangeNotifyEventHandler(OnModelChange);
+                SwEventPtr.FileOpenPostNotify -= new DSldWorksEvents_FileOpenPostNotifyEventHandler(FileOpenPostNotify);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public void AttachEventsToAllDocuments()
+        {
+            ModelDoc2 modDoc = (ModelDoc2)iSwApp.GetFirstDocument();
+            while (modDoc != null)
+            {
+                if (!openDocs.Contains(modDoc))
+                {
+                    AttachModelDocEventHandler(modDoc);
+                }
+                else if (openDocs.Contains(modDoc))
+                {
+                    bool connected = false;
+                    DocumentEventHandler docHandler = (DocumentEventHandler)openDocs[modDoc];
+                    if (docHandler != null)
+                    {
+                        connected = docHandler.ConnectModelViews();
+                    }
+                }
+
+                modDoc = (ModelDoc2)modDoc.GetNext();
+            }
+        }
+
+        public bool AttachModelDocEventHandler(ModelDoc2 modDoc)
+        {
+            if (modDoc == null)
+                return false;
+
+            DocumentEventHandler docHandler = null;
+
+            if (!openDocs.Contains(modDoc))
+            {
+                switch (modDoc.GetType())
+                {
+                    case (int)swDocumentTypes_e.swDocPART:
+                        {
+                            docHandler = new PartEventHandler(modDoc, this);
+                            break;
+                        }
+                    case (int)swDocumentTypes_e.swDocASSEMBLY:
+                        {
+                            docHandler = new AssemblyEventHandler(modDoc, this);
+                            break;
+                        }
+                    case (int)swDocumentTypes_e.swDocDRAWING:
+                        {
+                            docHandler = new DrawingEventHandler(modDoc, this);
+                            break;
+                        }
+                    default:
+                        {
+                            return false; //Unsupported document type
+                        }
+                }
+                docHandler.AttachEventHandlers();
+                openDocs.Add(modDoc, docHandler);
+            }
+            return true;
+        }
+
+        public bool DetachModelEventHandler(ModelDoc2 modDoc)
+        {
+            DocumentEventHandler docHandler;
+            docHandler = (DocumentEventHandler)openDocs[modDoc];
+            openDocs.Remove(modDoc);
+            modDoc = null;
+            docHandler = null;
+            return true;
+        }
+
+        public bool DetachEventHandlers()
+        {
+            DetachSwEvents();
+
+            //Close events on all currently open docs
+            DocumentEventHandler docHandler;
+            int numKeys = openDocs.Count;
+            object[] keys = new Object[numKeys];
+
+            //Remove all document event handlers
+            openDocs.Keys.CopyTo(keys, 0);
+            foreach (ModelDoc2 key in keys)
+            {
+                docHandler = (DocumentEventHandler)openDocs[key];
+                docHandler.DetachEventHandlers(); //This also removes the pair from the hash
+                docHandler = null;
+            }
+            return true;
+        }
+        #endregion
+
+        #region Event Handlers
+        //Events
+        public int OnDocChange()
+        {
+            return 0;
+        }
+
+        public int OnDocLoad(string docTitle, string docPath)
+        {
+            return 0;
+        }
+
+        int FileOpenPostNotify(string FileName)
+        {
+            AttachEventsToAllDocuments();
+            return 0;
+        }
+
+        public int OnFileNew(object newDoc, int docType, string templateName)
+        {
+            AttachEventsToAllDocuments();
+            return 0;
+        }
+
+        public int OnModelChange()
+        {
+            return 0;
+        }
+
+        #endregion
+    }
+}
